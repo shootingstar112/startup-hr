f7cb4e525e141a1bfa92088a49e3057b73f725a4
박준하 <metalmeldown1@gmail.com>
2026-02-10 13:00:16 +0900

    awerawer

--- app/resignation/page.tsx
+++ app/resignation/page.tsx
@@ -7,7 +7,7 @@ import UnemploymentBenefitCalculator from "./UnemploymentBenefitCalculator"; // 
 
 export default function Page() {
   return (
-    <main className="mx-auto max-w-5xl px-4 py-10">
+    <main className="mx-auto max-w-5xl px-2 py-10">
       <h1 className="text-3xl font-black">퇴사</h1>
 
       <div className="mt-6">
